14b891c230c3a52f4514825a0d068550c1072306
Anthony Doan <mythicalprogrammer@gmail.com>
2019-04-17 09:08:17 -0700

    Removed sinkall function since it is moved to util file.

--- XX3_kfold_results.R
+++ XX3_kfold_results.R
@@ -56,12 +56,3 @@ create_confusion_matrix <-
     print(confusionMatrix(pred, actual))
     sinkall()
   }
-
-sinkall <- function() {
-  i <- sink.number()
-  if (i > 0) {
-    for (i in 1:i) {
-      sink()
-    }
-  }
-}
